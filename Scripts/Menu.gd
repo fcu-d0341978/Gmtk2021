281844b403a778f525db45635ebe021d6f8d052e
Chowdog <yangerplato3465@gmail.com>
2021-08-15 22:38:41 +0800

    dd new ui

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -3,10 +3,6 @@ extends Node2D
 func _ready():
 	SignalManager.connect("transitioned", self, "change_scene")
 
-func _on_Button_pressed():
-	SignalManager.emit_signal("doTransition")
-	$CanvasLayer/Button/AudioStreamPlayer.play()
-
 func change_scene():
 #	var level = SaveState.loadGame()
 	
@@ -14,3 +10,8 @@ func change_scene():
 	get_tree().change_scene("res://Scene/Level24.tscn")
 #	else:
 #		get_tree().change_scene("res://Scene/Level" + String(level) + ".tscn")
+
+
+func _on_Start_pressed():
+	SignalManager.emit_signal("doTransition")
+	$AudioStreamPlayer.play()
