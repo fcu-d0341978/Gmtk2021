59a786933e73fa6aa58ea91fcded1fd38965a333
Chowdog <yangerplato3465@gmail.com>
2021-08-24 21:33:33 +0800

    moving block kill

--- Scripts/MovingBlock.gd
+++ Scripts/MovingBlock.gd
@@ -1,18 +1,31 @@
 extends Node2D
 
-const IDLE_DURATION = 1
+const BLOCK_SIZE = 32
 
-export var move_to = Vector2.RIGHT * 192
+export var move_to = Vector2.RIGHT * 2
 export var speed = 3.0
+export var idle_duration = 0.5
 
 onready var block = $KinematicBody2D
 onready var tween = $Tween
+onready var particlePosition = $KinematicBody2D/ParticlePosition
+
+var particleScene = preload("res://Prefabs/RedParticles.tscn")
 
 func _ready():
+	move_to = move_to * BLOCK_SIZE
 	init_tween()
 
 func init_tween():
-	var duration = move_to.length() / float(speed * 32)
-	tween.interpolate_property(block, "position", Vector2.ZERO, move_to, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, IDLE_DURATION)
-	tween.interpolate_property(block, "position", move_to, Vector2.ZERO, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, duration + IDLE_DURATION * 2)
+	var duration = move_to.length() / float(speed * BLOCK_SIZE)
+	tween.interpolate_property(block, "position", Vector2.ZERO, move_to, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, idle_duration)
+	tween.interpolate_property(block, "position", move_to, Vector2.ZERO, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, duration + idle_duration * 2)
 	tween.start()
+
+
+func _on_Area2D_area_entered(area):
+	if "Player" or "PlayerMirror" in area.name:
+		var particle = particleScene.instance()
+		add_child(particle)
+		particle.set_position(particlePosition.get_position())
+		SignalManager.emit_signal("doDeathTransition")
